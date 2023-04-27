dadb173e481d0d4da5ffde88bd29acc0ed4e6f08
Reinir <blackwhitenxt@gmail.com>
2023-04-27 10:18:50 +0700

    push data to db and get them back ok

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyBxEtjr3_OrTI194lOv68BgLmLpyV4RK4o",
   authDomain: "test-39037.firebaseapp.com",
@@ -11,6 +12,34 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+let db = getFirestore(app);
+
+import {
+  collection,
+  addDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+
+export function addU() {
+  addUser(prompt("Your name:"), prompt("Year of birth"));
+}
+
+async function addUser(name, yob) {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: name,
+      born: yob,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+const list = document.getElementById("list");
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  list.innerHTML +=
+    "<li>" + `${doc.data().first}, ${doc.data().born}` + "</li>";
+});
